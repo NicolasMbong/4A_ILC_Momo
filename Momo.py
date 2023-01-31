b3e0f127a3c15c0bb56e9b4c2eb8e137b8462b01
BenjaminDeschamps <deschampsbenjamin71@gmail.com>
2023-01-31 18:19:17 +0100

    change root request

--- Momo.py
+++ Momo.py
@@ -4,77 +4,89 @@ import csv
 
 
 app= Flask(__name__)
-@app.route("/")
+
 
 class Personne:
-        _nom =""
-        _solde =" "
+    _id = 0
+    _nom =""
+    _solde =" "
 
-        def __init__(self, nom,solde):
-                self._nom = nom
-                self._solde = solde
+    def __init__(self, nom,solde):
+        self._id +=1
+        self._nom = nom
+        self._solde = solde
 
-        def getNom(self):
-                return self._nom
+    def getId(self):
+        return self._id
 
-        def getSolde(self):
-                return self._solde
+    def getNom(self):
+        return self._nom
 
-        def setSolde(self,montant):
-                self._solde= self._solde + montant
-                return
+    def getSolde(self):
+        return self._solde
 
+    def setSolde(self,montant):
+        self._solde= self._solde + montant
 
-def addCompte(liste, Nom, solde):
-    for i in range(len(liste)):
-        if (liste[i].getNom()==Nom):
-            print("Un client avec les meme nom est deja dans la banque")
+Personnes=[]
+listTransaction=[]
+
+@app.route("/")
+def afficherClient():
+    affiche = "<h3>Liste de Personne :</h3>\n"
+    for personne in range(len(Personnes)):
+       	affiche += "<p>" + personne.toString() + "</p>\n"
+    return affiche
+
+@app.route("/addCompte/<string:nom>/<float:solde>")
+def addCompte(Nom, solde):
+    for i in range(len(Personnes)):
+        if (Personnes[i].getNom()==Nom):
+            return "Un client avec les meme nom est deja dans la banque"
+
     client=Personne(Nom,solde)
-    liste.append(client)
+    Personnes.append(client)
     print("le client a été ajouté a la banque")
-                        
-                    
+    return client
+
+
 def checktransaction(P1,transac):
     if (P1.getSolde() < transac) :
         return False
     return True
 
 
-def addTransaction(P1,P2,t,transfert):        
-    T=[P1,P2,t,transfert]
-    if (checktransaction(P1, transfert) == True) : 
-        print("La transaction d'un montant de:", transfert, "entre ", P1.getNom(), " et ", P2.getNom(), "à été effectué")
-        P1.setSolde(transfert)
-        P2.setSolde(-transfert)
-    else  :
-        print(P1, "ne peut pas transferer", transfert, "car son solde est trop faible")
+@app.route("/addTransaction/<int:id1>/<int:id2>/<float:transfert>")
+def addTransaction(id1,id2,transfert):
+    trans=[id1,id2,transfert]
+    if (checktransaction(Personnes[id1], transfert) == True) :
+        Personnes[id1].setSolde(transfert)
+        Personnes[id2].setSolde(-transfert)
+        listTransaction.append(trans)
+        return "La transaction d'un montant de:", transfert, "entre ", Personnes[id1].getNom(), " et ", Personnes[id2].getNom(), "à été effectué"
+    return Personnes[id1], "ne peut pas transferer", transfert, "car son solde est trop faible"
 
-
-def afficherTransactions(listClient):
-    for i in range(len(listClient)):
-        print(listClient[i])
+@app.route("/afficherTransaction/")
+def afficherTransaction():
+    for i in range(len(listTransaction)):
+        print(listTransaction[i])
 
 
 def trie(L):
     return sorted(L, key=lambda L: L[2])
 
 
-def afficherClient(M):
-    for i in range(len(M)):
-        print(M[i].getNom())
-    return
+addCompte("Max", 10)
+addCompte("Benjamin",1000)
+addCompte("Baptiste",0)
 
 
-Personnes=[]
-
-addCompte(Personnes,"Max", 10)
-addCompte(Personnes,"Benjamin",1000)
-addCompte(Personnes,"Baptiste",0)
-afficherClient(Personnes)
-
-addTransaction(Personnes[1],Personnes[0],8,100)
+afficherClient()
 
 
+addTransaction(1,0,100)
+addTransaction(1,2,10)
+afficherTransaction()
 
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
